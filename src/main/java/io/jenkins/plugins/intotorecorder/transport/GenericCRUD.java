99cf6e93e9c0e622d292511ed4f7eb278e409c25
Santiago Torres <torresariass@gmail.com>
2018-08-22 00:00:00 -0400

    ENH: transport: Transport
    To handle arbitrary transport protocols, we need an abstract handler and
    a transport package under the plugin's code. Add a transport package and
    a definition of the Abstract superclass for the handlers to inherrit.
    
    Likewise, add a method that arbitrarily instantiates known transports.

---
+++ src/main/java/io/jenkins/plugins/intotorecorder/transport/GenericCRUD.java
@@ -0,0 +1,45 @@
+/**
+ *
+ */
+package io.jenkins.plugins.intotorecorder.transport;
+
+import java.io.IOException;
+
+import io.in_toto.models.Link;
+
+import java.net.URI;
+
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.ByteArrayContent;
+import com.google.api.client.http.GenericUrl;
+
+
+public class GenericCRUD extends Transport {
+
+    URI uri;
+
+    public GenericCRUD(URI uri) {
+        this.uri = uri;
+    }
+
+    public void submit(Link link) {
+        try {
+            HttpRequest request = new NetHttpTransport()
+                .createRequestFactory()
+                .buildPutRequest(new GenericUrl(this.uri),
+                    ByteArrayContent.fromString("application/x-www-form-uriencoded", 
+                        link.toString()));
+            HttpResponse response = request.execute();
+            System.out.println(response.parseAsString());
+
+            /* FIXME: should handle error codes and other situations more appropriately,
+             * but this gets the job done for a PoC
+             */
+        } catch (IOException e) {
+            throw new RuntimeException("couldn't serialize to HTTP server");
+        }
+
+    }
+}
---
+++ src/main/java/io/jenkins/plugins/intotorecorder/transport/Transport.java
@@ -0,0 +1,39 @@
+/**
+ *
+ */
+package io.jenkins.plugins.intotorecorder.transport;
+
+import java.net.URI;
+
+import io.in_toto.models.Link;
+
+public abstract class Transport {
+
+    String uri;
+
+    public abstract void submit(Link link);
+
+    public static class TransportFactory {
+        public static Transport transportForURI(URI uri) {
+
+            if (uri == null)
+                return null;
+
+            if (uri.getScheme().equals("etcd")) {
+                return new Etcd(uri);
+            }
+
+            if (uri.getScheme().equals("redis")) {
+                return new Redis(uri);
+            }
+
+            if (uri.getScheme().equals("http") || uri.getScheme().equals("https")) {
+                return new GenericCRUD(uri);
+            }
+
+            throw new RuntimeException("This protocol (" + uri.getScheme()
+                + ") is not yet supported!");
+
+        }
+    }
+}
