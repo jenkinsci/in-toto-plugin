e6c460c78559cf9140931e0f0cf85300d28177c4
Santiago Torres <torresariass@gmail.com>
2018-08-24 00:00:00 -0400

    TEST: transport: Add Transport test
    To test the Transport instantiation class, we added a Transport unit
    test.

---
+++ src/test/java/io/jenkins/plugins/intotorecorder/transport/TransportTest.java
@@ -0,0 +1,45 @@
+/**
+ *
+ */
+package io.jenkins.plugins.intotorecorder.transport;
+
+import hudson.model.*;
+import junit.framework.TestCase;
+
+import java.net.URI;
+
+/**
+ *
+ * @author Santiago Torres-Arias
+ *
+ */
+public class TransportTest extends TestCase {
+
+	public void setUp() throws Exception {}
+
+	public void testTransportFactory() throws Exception {
+
+        Transport someTransport;
+        URI testUrl;
+
+        /* test creating etcd transports */
+        testUrl = new URI("etcd://localhost:4000/links/");
+        someTransport = Transport.TransportFactory.transportForURI(testUrl);
+        assertTrue(someTransport instanceof Etcd);
+
+        /* test creating Jedis transports */
+        testUrl = new URI("redis://localhost:4000/links/");
+        someTransport = Transport.TransportFactory.transportForURI(testUrl);
+        assertTrue(someTransport instanceof Redis);
+
+        /* test creating raw CRUD transports */
+        testUrl = new URI("http://localhost:4000/links/");
+        someTransport = Transport.TransportFactory.transportForURI(testUrl);
+        assertTrue(someTransport instanceof GenericCRUD);
+
+        testUrl = new URI("https://localhost:4000/links/");
+        someTransport = Transport.TransportFactory.transportForURI(testUrl);
+        assertTrue(someTransport instanceof GenericCRUD);
+
+	}
+}
